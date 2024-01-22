0fb302296e1a5922c9742f14d3cc2e0dd6222035
meesoee-deskTop <meesoeee@gmail.com>
2024-01-22 19:28:01 +0900

    헤더 작업 (진행중)
    https://webstoryboy.co.kr/1936 페이지 참고하여 작업중.
    본문 보면 모바일에서 메뉴를 토글하는 버튼을 설명하고 있는데
    굳이 햄버거 토글 버튼을 만들어야 하나 싶은 의문이 들어 일단 여기서 커밋.
    
    예제의 스크립팅을 모두 적용해두었고 css는 간단하게만 작업해두었음.
    - _vars
    - _reset
    - _header
    
    모바일에서 햄버거 메뉴가 나타나는 대신,
    로고 ㅡ 글로벌
    사이의 영역에서 페이지 이름이 보여지고, 스크롤 위치마다 이 이름이 바뀌되,
    다른 페이지로 이동하려면 요 페이지 이름을 클릭하면 드롭다운으로 다른 페이지 이름이 나오는건 어떤가 싶음.
    
    첫화면을 기준이라면
    About ▽ <- 이런식으로 아래화살표가 함께 있고 이걸 클릭하면
    --------
    Timeline
    Archive
    Contact
    
    이렇게 하위로 다른 페이지 이동 메뉴들이 나타나는 방식임.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+
+const pageList = [
+    {
+        title: "About",
+        url: "#about",
+    },
+    {
+        title: "Timeline",
+        url: "#timeline",
+    },
+    {
+        title: "Archive",
+        url: "#archive",
+    },
+    {
+        title: "contact",
+        url: "#contact",
+    },
+];
 
 const Header = () => {
+    const [nav_show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    };
+
     return (
-        <header>
-            <a href="/">LOGO</a>
-            <nav>Navigation</nav>
-            <a href="/">global</a>
-            <a href="/">TOP</a>
+        <header id="header" role="structure">
+            <div className="headerBox">
+                <div className="logo">
+                    <a href="/">LOGO</a>
+                </div>
+                <nav className={`nav ${nav_show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+                    <ul>
+                        {pageList.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <a id="global" href="/">global</a>
+                <a id="top" href="/">TOP</a>
+                <div 
+                    id="navToggle" 
+                    aria-controls="primary-menu"
+                    aria-expanded={nav_show ? "true" : "false"}
+                    role="button" 
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span>Mobile</span>
+                </div>
+            </div>
         </header>
     );
 };
