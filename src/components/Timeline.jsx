df95e3c2b5537c00759975a3352bb545eb1dacf1
meesoee-deskTop <meesoeee@gmail.com>
2024-02-02 17:00:47 +0900

    타임라인 데이터 배열 생성 및 출력

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 
+const portList = [
+    {
+        name: "밴드 적성 검사",
+        url: "#a",
+        year: "2021",
+    },
+    {
+        name: "디자인웍스",
+        url: "#b",
+        year: "2023",
+    },
+    {
+        name: "롤비티아이",
+        url: "#c",
+        year: "2023",
+    },
+    {
+        name: "리액트 사이트",
+        url: "#d",
+        year: "2024",
+    }
+];
+
 const Timeline = () => {
     return (
         <section className="Timeline" id="timeline">
+            <h3 className="section-caption">Timeline</h3>
             <div className="section-inner">
-                Timeline
+
+                {portList.map((ele) => (
+                    <a className="time-point"
+                    href={ele.url}
+                    title={ele.name + ' 포트폴리오로 이동'}>
+                        <div className="point-tooltip">
+                            <b>{ele.year + '년'}</b> {ele.name}
+                        </div>
+                    </a>
+                ))}
+                <div id="timeline-bar"></div>
             </div>
         </section>
     );
