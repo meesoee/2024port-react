1ac36edc7ba43da189b6b61434d095325b1f5715
meesoee-deskTop <meesoeee@gmail.com>
2024-02-04 20:57:28 +0900

    Timeline 데이터 수정 : 포폴 아닌것도 출력될 수 있도록 조건문처리
    - 포트폴리오 기준으로 타임포인트가 찍히는 마크업 구조였는데, 학교 졸업이력부터 근무이력까지 모두 출력시키기 위해 work 라는 키를 넣고 이 값을 판단하여 n일때 (=개인 포폴 작업일때)만 archive 섹션에 연결되도록 함.
    - 즉 학교 및 근무 이력은 별도 페이지를 만들지 않음.
    - 다만 타임라인 페이지가 여전히 허전해서 css를 좀 꾸며야할 필요는 있음.

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -2,21 +2,40 @@ import React from "react";
 
 const portList = [
     {
+        work: "school",
+        name: "백석대 시각디자인과 졸업",
+        year: "2019",
+    },
+    {
+        work: "y",
+        name: "키노티시스템 : 웹 디자이너 & 퍼블리셔",
+        year: "2019 ~ 2020",
+    },
+    {
+        work: "n",
         name: "밴드 적성 검사",
         url: "#bandtest",
         year: "2021",
     },
     {
+        work: "y",
+        name: "NHN커머스 : 솔루션 유지보수",
+        year: "2021 ~ 2023",
+    },
+    {
+        work: "n",
         name: "디자인웍스",
         url: "#designworks",
         year: "2023",
     },
     {
+        work: "n",
         name: "롤비티아이",
         url: "#lolbti",
         year: "2023",
     },
     {
+        work: "n",
         name: "리액트 사이트",
         url: "#react",
         year: "2024",
@@ -30,9 +49,25 @@ const Timeline = () => {
             <div className="section-inner">
 
                 {portList.map((ele,key) => (
+                    ele.work === 'n'
+                    ?
                     <a className="time-point"
                     href={ele.url}
                     title={ele.name + ' 포트폴리오로 이동'}
+                    key={key}>
+                        <div className="point-tooltip">
+                            <b>{ele.year + '년'}</b> {ele.name}
+                        </div>
+                    </a>
+                    :
+                    <a className="time-point"
+                    title={
+                        ele.work === 'y'
+                        ?
+                        ele.name + ' 근무'
+                        :
+                        ele.name
+                    }
                     key={key}>
                         <div className="point-tooltip">
                             <b>{ele.year + '년'}</b> {ele.name}
