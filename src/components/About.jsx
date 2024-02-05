7b4b60d54717b78507704b4f6f786cc5f1f27b8a
meesoee-deskTop <meesoeee@gmail.com>
2024-02-05 20:15:52 +0900

    About 한글 내용 추가 및 반응형 처리

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,17 @@ const About = () => {
                     <h1>MEESOEE</h1>
                     <p>a <span>WEB DEVELOPER</span>,<br />
                     formerly a <span>DESIGNER</span>.</p>
+                    <div className="kor">
+                        안녕하세요, <span>미소이</span>라고 합니다.
+                        <br />
+                        <span>디자이너</span>였기도 하고,
+                        <br />
+                        <span>웹 개발자</span>이기도 합니다.
+                        <br />
+                        <small><i>...아직은요!</i></small>
+                    </div>
                 </div>
+                <p>=== 아래로 스크롤 해서 구경하기 ===</p>
             </div>
         </section>
     );
