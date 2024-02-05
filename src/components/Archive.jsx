6c36eeb240c95eef731d562e4f2558721855a1af
meesoee-deskTop <meesoeee@gmail.com>
2024-02-05 16:35:07 +0900

    Archive 데이터 입력 + css 애니메이팅
    - 설명 및 기간 데이터 모두 입력
    - 참조레퍼런스 키가 있을 때만 노출되도록 date 밑에 마크업 추가 (리액트 포폴용)
    
    CSS 애니메이팅
    - 개별 포트폴리오 section에 마우스를 올릴 때 위치(margin)와 box-shadow가 움직이도록 함
    - 슬라이드 애니메이션을 대체하기 위해 slidebox에 마우스를 올렸을 때 첫번째 slideitem이 왼쪽으로 밀려나게하였음.
    - 즉 각 포트폴리오마다 최대 2개의 이미지를 넣을 수 있음.

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -23,26 +23,30 @@ const portList = [
         tag: ["github","html","css","javascript"],
         img: [""],
         alt: [""],
-        desc: "",
+        desc: "충주시에 위치한 인테리어디자인 업체의 홈페이지를 제작하였습니다.\n\n한 페이지 안에서 소개가 이루어져야하기에 랜딩페이지 스타일로 제작하였으며 상담 예약이 가능하도록 Formspree Api를 연결하여 문의 양식을 추가하였습니다. 또한, 개별 블로그의 최신 글을 가져올 수 있도록 RSS 를 호출하여 게시글 리스트가 출력될 수 있도록 XML을 가공하였습니다.\n\n국내 호스팅 업체를 통해 호스팅/배포하였으며 개발 코드 및 백업은 Github 레포지토리를 통해 관리하고 있습니다.",
         date: "2023년 10월 (7일 소요)",
+        url: "http://design-works.co.kr/",
     },
     {
         name: "롤비티아이",
         id: "lolbti",
-        tag: ["github","php","css","javascript"],
+        tag: ["github","php","css","javascript","mysql"],
         img: [""],
         alt: [""],
-        desc: "",
+        desc: "게임 '리그 오브 레전드'의 요소를 활용한 토이 프로젝트입니다. 그림 요소 및 기획은 담당자가 별도로 있으며, 본 프로젝트에서 저는 UI 디자인, DB 세팅, 풀스택 개발을 담당하였습니다.\n\n페이지 구성은 방문자가 테스트를 할 수 있는 프론트페이지와 관리자를 위한 어드민 페이지를 함께 개발하였으며 이를 위해 MySql 데이터베이스를 함께 구축해보았습니다.\n\n라이브러리나 프레임워크 없이 직접 어드민 페이지 개발이나 데이터베이스 환경을 모두 세팅하는 것은 처음이었기에 의미있었고 즐겁게 작업했던 프로젝트입니다.",
         date: "2023년 10월 ~ 12월 (약 2달 소요)",
+        url: "http://lolbti.com/"
     },
     {
         name: "리액트",
         id: "react",
-        tag: ["github","react","scss","javascript"],
+        tag: ["github","react","scss"],
         img: [""],
         alt: [""],
-        desc: "",
-        date: "",
+        desc: "현재까지의 작업물을 갈무리하기 위한 포트폴리오 페이지를 만들 겸, 한 번도 써보지 않은 라이브러리인 리액트를 사용해보았습니다.\n\n참고가 되는 페이지를 통해 기본적인 리액트 설치 및 netlify를 통한 배포 세팅을 한 뒤 자신만의 UI를 적용하여 만든 현재의 포트폴리오 사이트입니다.\n\n바닐라 자바스크립트를 어떻게 적용해야할지 몰라 애니메이션은 CSS trick만을 활용한 것이 특징입니다.\n\n님들아.. 리액트에서 자바스크립트 대체 어떻게 쓰는겁니까?",
+        date: "2024년 1월 ~ 2월 (약 10일 소요)",
+        url: "/",
+        ref: "https://webstoryboy.co.kr/1934",
     }
 ];
 
@@ -81,7 +85,14 @@ const Archive = () => {
                                 </div>
                             )}
                             <p className="desc">{ele.desc}</p>
-                            <p className="date mini">작업기간 : {ele.date}</p>
+                            <p className="mini">작업기간 : {ele.date}</p>
+                            {ele.ref && (
+                                ele.ref != false
+                                ?
+                                <p className="mini">참고자료 : <a href={ele.ref}>{ele.ref}</a></p>
+                                :
+                                ''
+                            )}
                             <button>
                                 <a href={ele.url} target="_blank">페이지 방문</a>
                             </button>
